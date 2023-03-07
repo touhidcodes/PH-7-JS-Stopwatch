f5a1227719c44355ce5dfbd0f7bf29f900f0b758
Touhidur Zaman <touhidcodes@gmail.com>
2023-03-07 20:51:35 +0600

    app-js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-document.getElementById("btn-start").addEventListener("click", () => {});
-document.getElementById("btn-pause").addEventListener("click", () => {});
-document.getElementById("btn-reset").addEventListener("click", () => {});
+let num = 0;
+const intervalId = setInterval(() => {
+	const value = ++num;
+	// console.log(value);
+	displayInterval("output", value);
+}, 1000);
+
+clearInterval(intervalId);
+
+const displayInterval = (id, value) => {
+	const displayId = document.getElementById(id);
+	displayId.innerText = value;
+};
+// document.getElementById("btn-start").addEventListener("click", () => {});
+// document.getElementById("btn-pause").addEventListener("click", () => {});
+// document.getElementById("btn-reset").addEventListener("click", () => {});
